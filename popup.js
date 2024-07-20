ee561055a43cd602887179d29df3f0b2b9d026ca
Sean Winnik <winnik.sean@gmail.com>
2024-07-20 12:33:56 -0700

    increment and reset to 0 button

--- popup.js
+++ popup.js
@@ -3,12 +3,20 @@
 // Initialize counter variable
 let counter = 0;
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('myButton').addEventListener('click', function() {
-        counter++;
 
-        // Update the displayed messages
+document.addEventListener('DOMContentLoaded', function() {
+    const incrementButton = document.getElementById('increment');
+    const resetButton = document.getElementById('resetButton');
+    // Set up the click event listener
+    incrementButton.addEventListener('click', function() {
+    counter++;
         document.getElementById('title').textContent = "Button Clicked!";
         document.getElementById('description').textContent = `You've clicked the button ${counter} times.`;
+    });
+
+    resetButton.addEventListener('click', function() {
+        counter = 0;
+        document.getElementById('title').textContent = "Counter Reset!";
+        document.getElementById('description').textContent = `Counter has been reset to ${counter}.`;
     });
 });
